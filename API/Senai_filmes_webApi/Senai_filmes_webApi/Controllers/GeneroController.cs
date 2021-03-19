2c16426e68647bc7f3d64274ea00133ca8fc6ab6
Matheus <mthneri@gmail.com>
2021-03-19 10:12:31 -0300

    Adicionando GeneroRepository e GeneroController de Generos usando exemplo de Filmes

---
+++ API/Senai_filmes_webApi/Senai_filmes_webApi/Controllers/GeneroController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai_filmes_webApi.Domains;
+using Senai_filmes_webApi.Interfaces;
+using Senai_filmes_webApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Controller responsável pelos endpoint (são os pontos onde será feito a requisição) referentes aos gêneros
+/// </summary>
+namespace Senai_filmes_webApi.Controllers
+{
+    // Define que o tipo de resposta da API será no formato "json"
+    [Produces("application/json")]
+
+    // Define que a rota de uma requisição será no formato domínio/api/nomeController
+    // ex: http://localhost:5000/api/generos
+    [Route("api/[controller]")]
+
+    // Define que é um controlador de API
+    [ApiController]
+    public class GeneroController : ControllerBase
+    {
+        /// <summary>
+        /// Objeto "_generoRepositoty" que irá receber todos os métodos definidos na interface "IGeneroRepository" 
+        /// </summary>
+        private IGeneroRepository _generoRepository { get; set; }
+
+        /// <summary>
+        /// Intancia o objeto "_generoRepositoty" para que haja a referência aos métodos no repositório
+        /// </summary>
+        public GeneroController()
+        {
+            _generoRepository = new GeneroRepository();
+
+        } // Fim do método construtor 
+
+        /// <summary>
+        /// Lista todos os gêneros
+        /// </summary>
+        /// <returns>Uma lista de gêneros e um status code</returns>
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            // Cria uma lista nomeada "ListaGeneros" para receber os dados
+            List<GeneroDomain> ListaGeneros = _generoRepository.ListarTodos();
+
+            // Retorna o status code 200 (Ok) com a lista de gêneros no formato JSON
+            return Ok(ListaGeneros);
+        }
+
+    } 
+}
---
+++ API/Senai_filmes_webApi/Senai_filmes_webApi/Interfaces/IFilmeRepository.cs
@@ -0,0 +1,29 @@
+﻿using Senai_filmes_webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_filmes_webApi.Interfaces
+{
+    public interface IFilmeRepository
+    {
+        // TipoRetorno NomeMetodo (TipoParametro NomeParametro)
+
+        List<FilmeDomain> ListarTodos(); // Método Read
+
+        FilmeDomain BuscarPorId(int id); // Método Read
+
+        void Cadastrar(FilmeDomain NovoFilme); // Método Create
+
+        void AtualizarIdCorpo(FilmeDomain movie); // Método Update
+
+        void AtualizarIdUrl(int id, FilmeDomain movie); // Método Update
+
+        void DeletarFilme(int id); // Método Delete
+
+
+
+
+    }
+}
