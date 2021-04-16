f1123c4a0add152397b6ce2ca4f0dddcc229e63b
Matheus Neri dos Santos <36773915805@info.local>
2021-04-16 17:10:38 -0300

    Modificando

--- Exercicio_People/API/Senai.Peoples.WebApi/Senai.Peoples.WebApi/Domains/PeopleDomain.cs
+++ Exercicio_People/API/Senai.Peoples.WebApi/Senai.Peoples.WebApi/Domains/PeopleDomain.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Senai.Peoples.WebApi.Domains
 {
@@ -13,7 +14,13 @@ namespace Senai.Peoples.WebApi.Domains
         
         public int IdFuncionario { get; set; }
         public string nome { get; set; }
+
+        [Required(ErrorMessage = "O sobrenome do funcionário é obrigatório!")]
+        [StringLength(10, MinimumLength = 5, ErrorMessage = "O sobrenome do funcionário deve conter de 5 a 10 caracteres.")]
         public string sobrenome { get; set; }
 
+        // DataType não valida campo, apenas especifica o tipo do campo
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; }
     }
 }
