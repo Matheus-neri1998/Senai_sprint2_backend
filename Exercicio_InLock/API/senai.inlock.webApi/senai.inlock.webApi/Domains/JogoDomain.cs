15f6765f77f91534c59474d7df1c1178e6844c7d
Matheus Neri dos Santos <36773915805@info.local>
2021-04-28 17:00:45 -0300

    Arquivo do projeto InLock iniciado

---
+++ Exercicio_InLock/API/senai.inlock.webApi/senai.inlock.webApi/Domains/JogoDomain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi_.Domains
+{
+    public class JogoDomain
+    {
+        // Coluna IdJogo
+        public int IdJogo { get; set; }
+
+        [Required(ErrorMessage = "O nome do jogo é obrigatório")]
+        // Coluna NomeJogo
+        public string NomeJogo { get; set; }
+
+        // Coluna Descricao
+        public string Descricao { get; set; }
+
+        [DataType(DataType.Date)]
+        // Coluna DataLancamento
+        public DateTime DataLancamento { get; set; }
+
+        // Coluna Valor
+        public double Valor { get; set; }
+
+        [Required(ErrorMessage = "O Id do estúdio desenvolvedor é obrigatório")]
+        // Coluna IdEstudio
+        public int IdEstudio { get; set; }
+    }
+}
