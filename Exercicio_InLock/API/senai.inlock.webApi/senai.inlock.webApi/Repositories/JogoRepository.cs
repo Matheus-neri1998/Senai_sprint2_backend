c68d77b571c818423789d56eb6cb356e6053b653
Matheus Neri dos Santos <36773915805@info.local>
2021-04-29 17:02:59 -0300

    Interfaces e Repositórios de Usuario e Jogo do Projeto InLock concluido

---
+++ Exercicio_InLock/API/senai.inlock.webApi/senai.inlock.webApi/Repositories/JogoRepository.cs
@@ -0,0 +1,77 @@
+﻿using senai.inlock.webApi_.Domains;
+using senai.inlock.webApi_.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi_.Repositories
+{
+    public class JogoRepository : IJogoRepository
+    {
+        private string StringConnection = "Data Source=MATHEUSNOTE\\SQLEXPRESS; initial catalog=InLock; integrated security=true";
+        public void Cadastrar(JogoDomain novoJogo)
+        {
+            using (SqlConnection con = new SqlConnection(StringConnection))
+            {
+                string InsertQuery = "INSERT INTO (Titulo) VALUES (@Titulo)";
+
+                using (SqlCommand cmd = new SqlCommand(InsertQuery, con))
+                {
+
+                        cmd.Parameters.AddWithValue("Titulo", novoJogo.NomeJogo);
+
+                        con.Open();
+
+                        cmd.ExecuteNonQuery();
+
+                }
+                
+            }
+        } // Fim do método Cadastrar
+
+        public List<JogoDomain> ListarTodos()
+        {
+            List<JogoDomain> ListaJogos = new List<JogoDomain>();
+
+            using (SqlConnection con = new SqlConnection(StringConnection))
+            {
+                string QuerySelectAll = "SELECT IdJogo, NomeJogo, Descricao, DataLancamento, Valor, IdEstudio FROM Jogos";
+
+                con.Open();
+
+                SqlDataReader Rdr;
+
+                using (SqlCommand cmd = new SqlCommand(QuerySelectAll, con))
+                {
+                    Rdr = cmd.ExecuteReader();
+
+                    while (Rdr.Read())
+                    {
+                        JogoDomain jogo = new JogoDomain()
+                        {
+                            IdJogo = Convert.ToInt32(Rdr[0]),
+
+                            NomeJogo = Rdr[1].ToString(),
+
+                            Descricao = Rdr[2].ToString(),
+
+                            DataLancamento = Convert.ToDateTime(Rdr[3]),
+
+                            Valor = Convert.ToDouble(Rdr[4]),
+
+
+                        };
+
+                        ListaJogos.Add(jogo);
+
+                    }
+                }
+            }
+
+            return (ListaJogos);
+            
+        } // Fim do método ListarTodos
+    }
+}
---
+++ Exercicio_InLock/API/senai.inlock.webApi/senai.inlock.webApi/Repositories/UsuarioRepository.cs
@@ -0,0 +1,53 @@
+﻿using senai.inlock.webApi_.Domains;
+using senai.inlock.webApi_.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi_.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private string StringConexao = "Data Source=MATHEUSNOTE\\SQLEXPRESS; initial catalog=InLock; integrated security=true";
+        public UsuarioDomain BuscarPorEmailSenha(string Email, string Senha)
+        {
+            using (SqlConnection con = new SqlConnection())
+            {
+                string QuerySelectAll = "SELECT IdUsuario, Email, Senha, IdTipoUsuario FROM Usuarios WHERE Email = @email AND Senha = @senha";
+
+                using (SqlCommand cmd = new SqlCommand(QuerySelectAll, con))
+                {
+                    cmd.Parameters.AddWithValue("@email", Email);
+                    cmd.Parameters.AddWithValue(Senha, Senha);
+
+                    con.Open();
+
+                    SqlDataReader rdr = cmd.ExecuteReader();
+
+                    if (rdr.Read())
+                    {
+                        UsuarioDomain UsuarioBuscado = new UsuarioDomain
+                        {
+                            IdUsuario = Convert.ToInt32(rdr["IdUsuario"]),
+                            Email = Convert.ToString(rdr["Email"]),
+                            Senha = Convert.ToString(rdr["Senha"]),
+                            IdTipoUsuario = Convert.ToInt32(rdr["IdTipoUsuario"])
+
+                        };
+
+
+                        return UsuarioBuscado;
+                        
+
+                    }
+
+                    return null;
+                }
+
+
+            }
+        }
+    }
+}
