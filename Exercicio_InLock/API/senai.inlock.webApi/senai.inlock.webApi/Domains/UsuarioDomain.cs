5ae671a2bc33bc137c35b05caebcf7390cb429c4
Matheus <mthneri@gmail.com>
2021-04-28 21:28:20 -0300

    API do InLock até a interface concluída

--- Exercicio_InLock/API/senai.inlock.webApi/senai.inlock.webApi/Domains/UsuarioDomain.cs
+++ Exercicio_InLock/API/senai.inlock.webApi/senai.inlock.webApi/Domains/UsuarioDomain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace senai.inlock.webApi_.Domains
 {
     public class UsuarioDomain
     {
+        // Coluna IdUsuario
+        public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "Informe o seu email")]
+        public string Email { get; set; } // Coluna Email
+
+        [Required(ErrorMessage = "Informe a sua senha")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "O campo senha´precisa ter no mínimo 3 e no máximo 20 caracteres")]
+        public string Senha { get; set; } // Coluna Senha
+
+        public int IdTipoUsuario { get; set; } // Coluna IdTipoUsuario
+
     }
 }
---
+++ Exercicio_InLock/API/senai.inlock.webApi/senai.inlock.webApi/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using senai.inlock.webApi_.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi_.Interfaces
+{
+    interface IUsuarioRepository
+    {
+        /// <summary>
+        /// Busca um usuário através de seu email e senha
+        /// </summary>
+        /// <param name="Email"></param>
+        /// <param name="Senha"></param>
+        /// <returns></returns>
+        UsuarioDomain BuscarPorEmailSenha(string Email, string Senha);
+    }
+}
