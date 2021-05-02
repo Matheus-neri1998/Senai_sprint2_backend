e09841e1849c678b75b0d002ef433fcb16f4eab8
Matheus <mthneri@gmail.com>
2021-05-02 18:16:54 -0300

    Configurando a Startup implementando Swagger

--- Exercicio_InLock/API/senai.inlock.webApi/senai.inlock.webApi/Startup.cs
+++ Exercicio_InLock/API/senai.inlock.webApi/senai.inlock.webApi/Startup.cs
@@ -3,10 +3,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace senai.inlock.webApi_
@@ -17,23 +21,56 @@ namespace senai.inlock.webApi_
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Define o uso de Controllers
+            services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Inlock.webApi", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
             services
-                // Adiciona o serviços de Controllers
-                .AddControllers()
-                .AddNewtonsoftJson(options =>
+                // Define a forma de autenticação
+                .AddAuthentication(options =>
                 {
-                    // Ignora os loopings nas consultas 
-                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.DefaultAuthenticateScheme = "JwtBearer";
+                    options.DefaultChallengeScheme = "JwtBearer";
+                })
 
+                // Define os parâmetros de validação do token
+                .AddJwtBearer("JwtBearer", options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // Quem está emitindo
+                        ValidateIssuer = true,
+
+                        // Quem está recebendo
+                        ValidateAudience = true,
+
+                        // Tempo de expiração será validado
+                        ValidateLifetime = true,
+
+                        // Forma de criptografia e a chave de autenticação
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("Inlock-chave-autenticacao")),
+
+                        // Tempo de expiração do token 
+                        ClockSkew = TimeSpan.FromMinutes(1),
+
+                        // Nome do issuer, de onde está vindo
+                        ValidIssuer = "Inlock.webApi",
+
+                        // Nome do audience, para onde está indo
+                        ValidAudience = "Inlock.webApi"
+                    };
                 });
-                
-
-
-
-
-        }   
-                
-
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -45,10 +82,28 @@ namespace senai.inlock.webApi_
 
             app.UseRouting();
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Inlock.webApi");
+                c.RoutePrefix = string.Empty;
+            });
+
+            // Habilita a autenticação
+            app.UseAuthentication();
+
+            // Habilita a autorização
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                // Define o mapeamento dos Controllers
                 endpoints.MapControllers();
             });
         }
-    }
+    } // Fim da Startup
 }
